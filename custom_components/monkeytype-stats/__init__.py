57753db0717e11a7e6e45018feb340f23586cd8b
wao6gx29 <140542009+wao6gx29@users.noreply.github.com>
2026-04-14 13:02:24 +0200

    test api

--- custom_components/monkeytype-stats/__init__.py
+++ custom_components/monkeytype-stats/__init__.py
@@ -1,29 +1,40 @@
 """ Monkeytype stats integration"""
 import aiohttp
+import asyncio
 
 base_url = "https://api.monkeytype.com" 
 ape_key = "cle_api"
 
+async def main()
+    async with aiohttp.ClientSession() as session:
+        async with session.get(base_url + '/public/typingStats') as resp:
+          print(resp.status)
+          print(await resp.test())
 
-public_stats = aiohttp.get(
-  base_url + '/public/typingStats')
-stats = public_stats.json()
-print(public_stats)
+asyncio.run(main())
 
 
-public_leaderboard = aiohttp.get(
-  base_url + '/leaderboards',
-  params={'language': 'english', 'mode': 'time', 'mode2': '60'}
-  )
-public_leaderboard = public_leaderboard.json()
-print(public_leaderboard)
 
-currentTestActivity = aiohttp.get(
-  base_url + '/users/currentTestActivity',
-  params={'language': 'english', 'mode': 'time', 'mode2': '60'},
-  headers={'Authorization': f'ApeKey {ape_key}'}
-  )
-message_erreur = currentTestActivity
-currentTestActivity = currentTestActivity.json()
-print(currentTestActivity)
-print(message_erreur)
+
+#public_stats = aiohttp.get(
+#  base_url + '/public/typingStats')
+#stats = public_stats.json()
+#print(public_stats)
+
+
+#public_leaderboard = aiohttp.get(
+#  base_url + '/leaderboards',
+#  params={'language': 'english', 'mode': 'time', 'mode2': '60'}
+#  )
+#public_leaderboard = public_leaderboard.json()
+#print(public_leaderboard)
+
+#currentTestActivity = aiohttp.get(
+#  base_url + '/users/currentTestActivity',
+#  params={'language': 'english', 'mode': 'time', 'mode2': '60'},
+#  headers={'Authorization': f'ApeKey {ape_key}'}
+#  )
+#message_erreur = currentTestActivity
+#currentTestActivity = currentTestActivity.json()
+#print(currentTestActivity)
+#print(message_erreur)
