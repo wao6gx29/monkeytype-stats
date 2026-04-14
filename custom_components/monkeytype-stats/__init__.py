6d89ca2fbdd4b55b6df9d9abe792328287aa93b8
wao6gx29 <140542009+wao6gx29@users.noreply.github.com>
2026-04-14 12:58:02 +0200

    change from requests to aiohttp

--- custom_components/monkeytype-stats/__init__.py
+++ custom_components/monkeytype-stats/__init__.py
@@ -5,20 +5,20 @@ base_url = "https://api.monkeytype.com"
 ape_key = "cle_api"
 
 
-public_stats = requests.get(
+public_stats = aiohttp.get(
   base_url + '/public/typingStats')
 stats = public_stats.json()
 print(public_stats)
 
 
-public_leaderboard = requests.get(
+public_leaderboard = aiohttp.get(
   base_url + '/leaderboards',
   params={'language': 'english', 'mode': 'time', 'mode2': '60'}
   )
 public_leaderboard = public_leaderboard.json()
 print(public_leaderboard)
 
-currentTestActivity = requests.get(
+currentTestActivity = aiohttp.get(
   base_url + '/users/currentTestActivity',
   params={'language': 'english', 'mode': 'time', 'mode2': '60'},
   headers={'Authorization': f'ApeKey {ape_key}'}
